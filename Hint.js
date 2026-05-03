bec6c88a3ed74150c13d7c052320f3ab309d72dc
Steven Saus <steven@stevesaus.com>
2026-05-03 12:28:31 -0400

    - Adds `--player|...` targeting by player display name.  - Adds `--token|...` targeting by token name, resolving controlling players.  - Resolves all hint recipients to concrete player IDs before calling `sendPing()`.  - Sends character-targeted whispers to controlling players instead of character names.  - Makes Re-Ping self-contained by storing the source token ID and resolved player IDs.  - Escapes the Re-Ping command payload so quoted hint text does not break the chat button.  - Updates the help output for the new targeting options.  - Fixes GM whisper `noarchive` handling.

--- Hint.js
+++ Hint.js
@@ -2,11 +2,21 @@
 Hint
 Sends a location ping and hint for Roll20 games
 
+Original script by blawson69 
 On Github:	https://github.com/blawson69
 Contact me: https://app.roll20.net/users/1781274/ben-l
+Patreon: https://www.patreon.com/benscripts
 
-Like this script? Become a patron:
-    https://www.patreon.com/benscripts
+This fork changes the following: 
+ - Adds `--player|...` targeting by player display name.
+ - Adds `--token|...` targeting by token name, resolving controlling players.
+ - Resolves all hint recipients to concrete player IDs before calling `sendPing()`.
+ - Sends character-targeted whispers to controlling players instead of character names.
+ - Makes Re-Ping self-contained by storing the source token ID and resolved player IDs.
+ - Escapes the Re-Ping command payload so quoted hint text does not break the chat button.
+ - Updates the help output for the new targeting options.
+ - Fixes GM whisper `noarchive` handling.
+
 */
 
 var Hint = Hint || (function () {
@@ -14,7 +24,7 @@ var Hint = Hint || (function () {
 
     //---- INFO ----//
 
-    var version = '0.1',
+    var version = '0.2',
     debugMode = false,
     styles = {
         box:  'background-color: #fff; border: 1px solid #000; padding: 8px 10px; border-radius: 6px; margin-left: -40px; margin-right: 0px;',
@@ -48,50 +58,101 @@ var Hint = Hint || (function () {
     },
 
     commandHint = function (msg) {
-		// Send hint with ping to designated players
-		if (!msg.selected || msg.selected.length > 1) {
-			showDialog('Hint Error', 'You must select one token. No more, no less.', 'GM');
-			return;
-		}
+		var args = msg.content.split(/\s*\-\-/i);
+        var hint = '', char_ids = [], player_ids = [], token_players = [], visible_to = [], re_ping = false, location_id = '';
+        var location_token = null;
 
-		var location_token = getObj(msg.selected[0]._type, msg.selected[0]._id), args = msg.content.split(/\s*\-\-/i);
-        var hint = '', char_ids = [], re_ping = false;
-
-        if (location_token) {
-            _.each(args, function (arg) {
+        _.each(args, function (arg) {
                 if (arg.startsWith('msg|')) {
                     hint = arg.replace('msg|', '');
                 }
                 if (arg.startsWith('to|')) {
                     var char_list = arg.replace('to|', '').split(/\s*\,\s*/);
-                    char_ids = getIDsFromNames(char_list, location_token.get('pageid'));
+                    char_ids = char_ids.concat(getCharacterIDsFromNames(char_list));
+                }
+                if (arg.startsWith('player|')) {
+                    var player_list = arg.replace('player|', '').split(/\s*\,\s*/);
+                    player_ids = player_ids.concat(getPlayerIDsFromNames(player_list));
+                }
+                if (arg.startsWith('token|')) {
+                    var token_list = arg.replace('token|', '').split(/\s*\,\s*/);
+                    token_players = token_players.concat(token_list);
+                }
+                if (arg.startsWith('location|')) {
+                    location_id = arg.replace('location|', '');
+                }
+                if (arg.startsWith('visibleto|')) {
+                    visible_to = visible_to.concat(arg.replace('visibleto|', '').split(/\s*\,\s*/));
                 }
                 if (arg.startsWith('re-ping')) {
                     re_ping = true;
                 }
             });
 
-            if (_.size(char_ids) > 0 && hint != '') {
-                var players = [];
-                _.each(char_ids, function (char_id) {
-                    var char = getObj('character', char_id);
-                    var tmp_players = char.get('controlledby');
-                    if (tmp_players != 'all') players.push(tmp_players.split(','));
-                });
-                players = _.without(_.unique(_.flatten(players)), 'all');
+		if (location_id) {
+            location_token = getObj('graphic', location_id);
+        } else if (msg.selected && msg.selected.length === 1) {
+            location_token = getObj(msg.selected[0]._type, msg.selected[0]._id);
+        } else if (!msg.selected || msg.selected.length > 1) {
+			showDialog('Hint Error', 'You must select one token. No more, no less.', 'GM');
+			return;
+		}
 
-                if (!re_ping) {
+        if (location_token) {
+            token_players = getPlayerIDsFromTokens(token_players, location_token.get('pageid'));
+            char_ids = _.uniq(char_ids);
+            player_ids = _.uniq(player_ids);
+            token_players = _.uniq(token_players);
+
+            if (hint != '') {
+                var players = _.uniq(_.without(_.flatten(visible_to), 'all', ''));
+                if (!players.length) {
+                    players = [].concat(player_ids, token_players);
+                    _.each(char_ids, function (char_id) {
+                        var char = getObj('character', char_id);
+                        players = players.concat(getControlledPlayerIDs(char));
+                    });
+                    players = _.uniq(_.without(_.flatten(players), 'all', ''));
+                }
+
+                if (players.length) {
                     var names = [];
                     _.each(char_ids, function (char_id) {
                         var name = getNameFromID(char_id);
                         var subtitle = '<div style=\'' + styles.character + '\'>' + name + '</div>';
+                        var char_players = getControlledPlayerIDs(getObj('character', char_id));
                         names.push(name);
-                        showDialog('', subtitle + hint, name);
+                        _.each(char_players, function (player_id) {
+                            var player_name = getPlayerNameFromID(player_id);
+                            if (player_name) showDialog('', subtitle + hint, player_name);
+                        });
                     });
-                    var message = '"' + hint + '" was whispered to ' + grammaticallyCorrect(names) + '.<br><a style=\'' + styles.button + '\' href="' + msg.content + ' --re-ping">Re-Ping</a>';
-                    showDialog('Hint Delivered', message, 'GM');
+                    _.each(player_ids, function (player_id) {
+                        var player_name = getPlayerNameFromID(player_id);
+                        if (player_name) {
+                            names.push(player_name);
+                            showDialog('', hint, player_name);
+                        }
+                    });
+                    _.each(token_players, function (player_id) {
+                        var token_player_name = getPlayerNameFromID(player_id);
+                        if (token_player_name && !_.contains(player_ids, player_id)) {
+                            names.push(token_player_name);
+                            showDialog('', hint, token_player_name);
+                        }
+                    });
+                    names = _.uniq(names);
+                    if (!re_ping) {
+                        var re_ping_command = getRePingCommand(hint, players, location_token.id);
+                        var message = '"' + hint + '" was whispered to ' + grammaticallyCorrect(names) + '.<br><a style=\'' + styles.button + '\' href="' + escapeHtmlAttribute(re_ping_command) + '">Re-Ping</a>';
+                        showDialog('Hint Delivered', message, 'GM');
+                    }
                 }
-                sendPing(location_token.get('left'), location_token.get('top'), location_token.get('pageid'), null, true, players);
+                if (players.length) {
+                    sendPing(location_token.get('left'), location_token.get('top'), location_token.get('pageid'), null, true, players);
+                } else {
+                    showDialog('Hint Error', 'You must provide a hint message and at least one valid recipient.', 'GM');
+                }
             } else {
                 showDialog('Hint Error', 'You must provide a hint message and at least one valid recipient.', 'GM');
             }
@@ -104,14 +165,18 @@ var Hint = Hint || (function () {
         // Show help dialog
         var message = 'Command format:<br>';
         message += '<div style=\'' + styles.code + '\'>!hint --msg|&lt;message_text&gt; --to|&lt;character_names&gt;</div>';
+        message += '<div style=\'' + styles.code + '\'>!hint --msg|&lt;message_text&gt; --player|&lt;player_names&gt;</div>';
+        message += '<div style=\'' + styles.code + '\'>!hint --msg|&lt;message_text&gt; --token|&lt;token_names&gt;</div>';
         message += '<b style=\'' + styles.code + '\'>&lt;message_text&gt;:</b><br>The text of your message. No double dashes allowed.<br><br>';
         message += '<b style=\'' + styles.code + '\'>&lt;character_names&gt;:</b><br>Comma-delimited list of character names. May be partial names. Capitalization ignored.<br><br>';
+        message += '<b style=\'' + styles.code + '\'>&lt;player_names&gt;:</b><br>Comma-delimited list of player display names. May be partial names. Capitalization ignored.<br><br>';
+        message += '<b style=\'' + styles.code + '\'>&lt;token_names&gt;:</b><br>Comma-delimited list of token names on the current page. If a token represents a character, the character controllers are used first.<br><br>';
         message += 'You <b>must</b> have a token selected for the ping location reference.<br><br>';
         message += 'See the <a style="' + styles.textButton + '" href="https://github.com/blawson69/Hint">documentation</a> for complete instructions.';
         showDialog('Help Menu', message, 'GM');
     },
 
-    getIDsFromNames = function (names, page_id) {
+    getCharacterIDsFromNames = function (names) {
         var chars = [];
         var allChars = findObjs({type: 'character', archived: false}, {caseInsensitive: true});
         _.each(names, function (name) {
@@ -121,10 +186,62 @@ var Hint = Hint || (function () {
         return chars;
     },
 
+    getPlayerIDsFromNames = function (names) {
+        var players = [];
+        var allPlayers = findObjs({type: 'player'});
+        _.each(names, function (name) {
+            var player = _.find(allPlayers, function (x) { return x.get('_displayname').toLowerCase().search(name.toLowerCase()) > -1; });
+            if (player) players.push(player.get('id'));
+        });
+        return players;
+    },
+
+    getPlayerIDsFromTokens = function (names, page_id) {
+        var tokens = findObjs({type: 'graphic', subtype: 'token', pageid: page_id}, {caseInsensitive: true});
+        var players = [];
+        _.each(names, function (name) {
+            var token = _.find(tokens, function (x) { return x.get('name').toLowerCase().search(name.toLowerCase()) > -1; });
+            if (token) players = players.concat(getControlledPlayerIDsFromToken(token));
+        });
+        return players;
+    },
+
+    getControlledPlayerIDsFromToken = function (token) {
+        var represented = token.get('represents');
+        if (represented) {
+            var char = getObj('character', represented);
+            if (char) return getControlledPlayerIDs(char);
+        }
+        return getControlledPlayerIDs(token);
+    },
+
+    getControlledPlayerIDs = function (obj) {
+        var controlled_by = obj && obj.get('controlledby');
+        if (!controlled_by || controlled_by == 'all') return [];
+        return controlled_by.split(',');
+    },
+
     getNameFromID = function (char_id) {
         var name = 'Unknown Character', char = getObj('character', char_id);
         if (char) name = char.get('name');
         return name;
+    },
+
+    getPlayerNameFromID = function (player_id) {
+        var player = getObj('player', player_id);
+        return player ? player.get('_displayname') : '';
+    },
+
+    getRePingCommand = function (hint, players, location_id) {
+        return '!hint --msg|' + hint + ' --visibleto|' + players.join(',') + ' --location|' + location_id + ' --re-ping';
+    },
+
+    escapeHtmlAttribute = function (text) {
+        return text
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
     },
 
 	grammaticallyCorrect = function (names) {
@@ -139,11 +256,12 @@ var Hint = Hint || (function () {
     showDialog = function (title, content, whisperTo = '') {
         // Outputs a pretty box in chat with a title and content
         var gm = /\(GM\)/i;
+        var whisperTarget = whisperTo;
         title = (title == '') ? '' : '<div style=\'' + styles.title + '\'>' + title + '</div>';
         var body = '<div style=\'' + styles.box + '\'>' + title + '<div>' + content + '</div></div>';
         if (whisperTo.length > 0) {
             whisperTo = '/w ' + (gm.test(whisperTo) ? 'GM' : '"' + whisperTo + '"') + ' ';
-            sendChat('Hint', whisperTo + body, null, {noarchive:(whisperTo == 'GM')});
+            sendChat('Hint', whisperTo + body, null, {noarchive: gm.test(whisperTarget)});
         } else  {
             sendChat('Hint', body);
         }
--- README.md
+++ README.md
@@ -1,16 +1,26 @@
 # Hint
-Hint is a [Roll20](http://roll20.net/) script that allows the GM to send a hint to designated characters along with a ping to a map location. As an example, you can whisper the message "You notice a loose tile..." to every player whose character has a high enough passive Perception check to discover your trap, and only those players will see a ping in the location of said tile.
+Hint is a [Roll20](http://roll20.net/) script that allows the GM to send a hint to designated characters, players, or token controllers along with a ping to a map location. As an example, you can whisper the message "You notice a loose tile..." to every player whose character has a high enough passive Perception check to discover your trap, and only those players will see a ping in the location of said tile.
 
-This script takes advantage of the new `visibleTo` parameter of the [sendPing API function](https://wiki.roll20.net/API:Utility_Functions#Miscellaneous) to only ping the players who control those characters.
+This script takes advantage of the `visibleTo` parameter of the [sendPing API function](https://wiki.roll20.net/API:Utility_Functions#Miscellaneous) to only ping the players who control those characters.
 
 ## Use
+
 Place a token in the location where you want the ping to originate. This can be any graphic, including a transparent one. Select this token and send the `!hint` command along with the necessary parameters. Format your command thus:
 ```
 !hint --msg|<message-text> --to|<character-names>
+!hint --msg|<message-text> --player|<player-names>
+!hint --msg|<message-text> --token|<token-names>
 ```
-The Hint script will ping the designated players, and will move the map to the location of the ping for each of them as well (as if long-clicking with the Shift key depressed). Players who do not control a character designated in the command will see neither the ping nor the whispered message.
+The Hint script will ping the designated players, and will move the map to the location of the ping for each of them as well (as if long-clicking with the Shift key depressed). Players who are not targeted by the command will see neither the ping nor the whispered message.
+
+### With A Macro
+
+Create a macro with the *same text* you would use if typing the command.  You want to select "Show as Token Action" so you don't forget that you have to select something first.  
+
+![2026-05-03_12.05.48](https://i.imgur.com/hu0B60n.png)
 
 ### Message
+
 The text that follows `--msg|` can contain normal punctuation, but should avoid double dashes (--). This message will be whispered to each player that controls a character whose name you provide. Because of the intended use of the script, it will ignore characters who are controlled by all players.
 
 The GM will receive a whisper with the original message and a list of the proper character names to whom the message was whispered.
@@ -20,8 +30,16 @@ The character names is a comma-delimited list of names that follows the `--to|` 
 
 Be careful that you don't pass a name fragment that will match for multiple characters, as the script will only take the first match and other matches will be ignored. Sending the name "cor" will match both "Rancor the Pungent" and "Albacore the Tuna" but will only use the first match for the hint message/ping.
 
+### Player Names
+The player names is a comma-delimited list of display names that follows the `--player|` parameter. These names can be shortened versions of the player's display name, and capitalization does not matter.
+
+### Token Names
+The token names is a comma-delimited list of token names on the current page that follows the `--token|` parameter. These names can be shortened versions of the token's name, and capitalization does not matter.
+
+If a token represents a character, Hint uses that character's `controlledby` list first. Otherwise it falls back to the token's own `controlledby` list. This matches the common Roll20 setup where the controlling players are stored on the represented character rather than the token itself.
+
 ### Re-Pinging
-The message received by the GM will contain a "Re-Ping" button that will allow you to re-send a ping to the players. This is helpful for when a player forgot the location of the ping or somehow missed seeing the original. You just know it'll happen. Note that the ping *will follow* the selected token, so if the token moves, so will the ping.
+The message received by the GM will contain a "Re-Ping" button that will allow you to re-send a ping to the players. This is helpful for when a player forgot the location of the ping or somehow missed seeing the original. You just know it'll happen. The button stores the original ping token, so if that token moves before you click Re-Ping, the new ping will follow its updated position.
 
 ## Help
 Sending `!hint` with no parameters will display a Help dialog to the GM with simple directions for using the script.
